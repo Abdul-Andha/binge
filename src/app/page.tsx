63a409ecac495eaf67f3f6bc2a9d6ab0e1d0bcd4
shehabmohsen <aquashehab612@gmaiil.com>
2024-10-05 13:46:09 -0400

    feat: upload endpoint boilerplate code

---
+++ src/app/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState } from "react";
+
+export default function Home() {
+  const [file, setFile] = useState<File>();
+  const [url, setUrl] = useState("");
+  const [uploading, setUploading] = useState(false);
+
+  const uploadFile = async () => {
+    try {
+      if (!file) {
+        alert("No file selected");
+        return;
+      }
+
+      setUploading(true);
+      const data = new FormData();
+      data.set("file", file);
+      const uploadRequest = await fetch("/api/files", {
+        method: "POST",
+        body: data,
+      });
+      const signedUrl = await uploadRequest.json();
+      console.log(signedUrl)
+      setUrl(signedUrl);
+      setUploading(false);
+    } catch (e) {
+      console.log(e);
+      setUploading(false);
+      alert("Trouble uploading file");
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFile(e.target?.files?.[0]);
+  };
+
+  return (
+    <main className="w-full min-h-screen m-auto flex flex-col justify-center items-center">
+      <input type="file" onChange={handleChange} />
+      <button disabled={uploading} onClick={uploadFile}>
+        {uploading ? "Uploading..." : "Upload"}
+      </button>
+      {url && <img src={url} alt="Image from Pinata" />}
+    </main>
+  );
+}
