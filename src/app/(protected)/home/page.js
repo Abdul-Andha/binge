029e198fd234c6a015d117f5f6d8a8360c367ab7
Abdul-Andha <aandha8342@gmail.com>
2024-10-05 18:13:50 -0400

    rebase

--- src/app/(protected)/home/page.js
+++ src/app/(protected)/home/page.js
@@ -1,5 +1,24 @@
-export default function Home(){
+"use client";
+
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+    const router = useRouter();
+
+    const handleNavigation = () => {
+        router.push('/livestream');
+    };
+
     return (
-        <div>Home</div>
-    )
+        <div className="flex flex-col items-center">
+            <h1 className="text-2xl mb-4">Home</h1>
+            {/* Button to navigate to the livestream page */}
+            <button
+                onClick={handleNavigation}
+                className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600"
+            >
+                Go to Livestream
+            </button>
+        </div>
+    );
 }
