dfc4ced9e69ce719cb5d60c712a72de5e775ef66
shehabmohsen <aquashehab612@gmaiil.com>
2024-10-06 06:18:13 -0400

    feat: hardcode audio player

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,14 +5,14 @@ import { NewsKitProvider, newskitLightTheme } from "newskit";
 
 export default function RootLayout({ children }) {
   return (
-    // <NewsKitProvider
-    //   theme={newskitLightTheme}
-    //   instrumentation={"instrumentation provider props"}
-    //   layer={"layer organizer props"}
-    // >
+    <NewsKitProvider
+      theme={newskitLightTheme}
+      instrumentation={"instrumentation provider props"}
+      layer={"layer organizer props"}
+    >
       <html lang="en">
       <body className="w-full h-screen p-4">{children}</body>
       </html>
-    // </NewsKitProvider>
+    </NewsKitProvider>
   );
 }
