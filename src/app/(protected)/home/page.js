ab112b0ca8f1d959bfac3f07cab0892aefa104c2
shehabmohsen <aquashehab612@gmaiil.com>
2024-10-06 00:22:28 -0400

    feat: newskit ui scaffolding

--- src/app/(protected)/home/page.js
+++ src/app/(protected)/home/page.js
@@ -1,4 +1,18 @@
+'use client'
+
+import { useEffect } from "react"
 export default function Home(){
+    useEffect(()=>{
+        const getAudioFile = async () => {
+            const uploadRequest = await fetch("/api/files/get", {
+                method: "GET",
+                // body: 
+            });
+            const responseData = await uploadRequest.json()
+            console.log(responseData)
+        }
+    getAudioFile()
+},[])
     return (
         <div>Home</div>
     )
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,18 @@
 "use client";
-import { NewsKitProvider, Tag, newskitLightTheme } from "newskit";
+import { NewsKitProvider, newskitLightTheme } from "newskit";
 
 
 
 export default function RootLayout({ children }) {
   return (
-    <NewsKitProvider
-      theme={newskitLightTheme}
-      instrumentation={"instrumentation provider props"}
-      layer={"layer organizer props"}
-    >
+    // <NewsKitProvider
+    //   theme={newskitLightTheme}
+    //   instrumentation={"instrumentation provider props"}
+    //   layer={"layer organizer props"}
+    // >
       <html lang="en">
       <body className="w-full h-screen p-4">{children}</body>
       </html>
-    </NewsKitProvider>
+    // </NewsKitProvider>
   );
 }
